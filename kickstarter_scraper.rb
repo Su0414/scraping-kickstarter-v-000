4e4f23e36fd6c831f7f20f2d1f3074b85426a2d6
Suchitra Naik <susanaik@gmail.com>
2018-05-09 13:34:12 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,5 +4,6 @@ def create_project_hash
   # write your code here
   
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML()
+  kickstarter = Nokogiri::HTML(html)
+  
 end
