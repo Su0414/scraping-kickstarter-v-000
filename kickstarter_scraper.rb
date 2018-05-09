e8cc2f65b330ecdf697606f2e013f1cb5df707a0
Suchitra Naik <susanaik@gmail.com>
2018-05-09 14:15:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,12 +3,6 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-  # write your code here
-  
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry 
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # project.css("div.project-thumbnail a img").attribute("src").value
@@ -17,6 +11,17 @@ def create_project_hash
   
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
+
+def create_project_hash
+  # write your code here
+  
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry 
+  
+  projects = {}
+  
+ 
   
 end
 
